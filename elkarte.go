25b60e2df432ae382f20432074cd54e2bccf2a96
Antony Derham <antony@xydre.com>
2015-09-02 13:10:53 +0100

    Fleshed out theme and Elkarte options

--- elkarte.go
+++ elkarte.go
@@ -58,8 +58,53 @@ func main() {
 					println("Ready!")
 				case "theme":
 					println("Creating new theme")
+					name := c.Args()[1]
+					os.Mkdir(name, 0755)
+					os.Chdir(name)
+
+					cmd := "git"
+					args := []string{"clone", "https://github.com/Ant59/elkarte-vagrant-theme.git", "."}
+					if err := exec.Command(cmd, args...).Run(); err != nil {
+						fmt.Fprintln(os.Stderr, err)
+						os.Exit(1)
+					}
+					println("Successfully fetched environment")
+					println("Setting up...")
+					os.Mkdir(name, 0755)
+
+					read, err := ioutil.ReadFile("Vagrantfile")
+					if err != nil {
+						panic(err)
+					}
+
+					newContents := strings.Replace(string(read), "theme_name", name, -1)
+
+					err = ioutil.WriteFile("Vagrantfile", []byte(newContents), 0)
+					if err != nil {
+						panic(err)
+					}
+					println("Ready!")
 				case "elkarte":
-					println("Fetching Elkarte")
+					println("Fetching Elkarte and setting up environment")
+					name := c.Args()[1]
+					os.Mkdir(name, 0755)
+					os.Chdir(name)
+
+					cmd := "git"
+					args := []string{"clone", "https://github.com/Ant59/elkarte-vagrant.git", "."}
+					if err := exec.Command(cmd, args...).Run(); err != nil {
+						fmt.Fprintln(os.Stderr, err)
+						os.Exit(1)
+					}
+					println("Successfully fetched environment")
+					cmd = "git"
+					args = []string{"clone", "https://github.com/elkarte/Elkarte.git"}
+					if err := exec.Command(cmd, args...).Run(); err != nil {
+						fmt.Fprintln(os.Stderr, err)
+						os.Exit(1)
+					}
+					println("Successfully fetched Elkarte")
+					println("Ready!")
 				default:
 					println("You must enter 'addon', 'theme' or 'elkarte'")
 				}
